2716df0092dec08f16cc5b0cef7b6e747703a146
Renata Nunes <renatanunes231@yahoo.com.br>
2022-02-16 16:26:37 -0300

    React application started and page structure, routes and contextApi configured

---
+++ task-organize-frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+import Provider from './context/Provider';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
---
+++ task-organize-frontend/src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <h2>Página não encontrada</h2>
+    </div>
+  );
+}
+
+export default NotFound;
