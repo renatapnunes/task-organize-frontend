4d99eb5a47f96c4b8adf158107b85156799b6a61
Renata Nunes <renatanunes231@yahoo.com.br>
2022-02-16 16:59:54 -0300

    Configured API requests

---
+++ task-organize-frontend/src/services/api.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const api = axios.create({ baseURL: 'http://localhost:3001' });
+
+const http = {
+  getTasks: async () => {
+    try {
+      const response = await api.get('/task');
+      return response.data;
+    } catch (error) {
+      return error.response.status;
+    }
+  },
+  createTask: async ({ task, status }) => {
+    try {
+      const response = await api.post('/task', { task, status });
+      return response.data;
+    } catch (error) {
+      return error.response.status;
+    }
+  },
+  updateTask: async ({ id, task, status, created }) => {
+    try {
+      const response = await api.put(`/task/${id}`, { task, status, created });
+      return response.data;
+    } catch (error) {
+      return error.response.status;
+    }
+  },
+  deleteTask: async ({ id }) => {
+    try {
+      const response = await api.delete(`/task/${id}`, {});
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  },
+  deleteAllTasks: async () => {
+    try {
+      const response = await api.delete('/task', {});
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  },
+};
+
+export default http;
