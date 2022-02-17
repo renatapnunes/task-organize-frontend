153f415b6bd1bdb477c59b4fd28979f541f107cd
Renata Nunes <renatanunes231@yahoo.com.br>
2022-02-17 08:37:27 -0300

    Styling application with css

--- task-organize-frontend/src/components/Task.js
+++ task-organize-frontend/src/components/Task.js
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 
 import Context from '../context/Context';
 import http from '../services/api';
+import editIcon from '../images/edit.png';
+import deleteIcon from '../images/delete.png';
+
+const statusColor = {
+  PENDENTE: '#fab2b2',
+  'EM ANDAMENTO': '#ffe69b',
+  PRONTO: '#3fccbc',
+};
 
 function Task({ taskData }) {
   const { _id, task, status, created } = taskData;
@@ -15,21 +23,34 @@ function Task({ taskData }) {
   };
 
   return (
-    <li value={ _id }>
-      <div>{ task }</div>
-      <div>{ created }</div>
-      <div>{ status }</div>
+    <li
+      className="task-card"
+      style={ { border: `0.2em solid ${statusColor[status]}` } }
+      value={ _id }
+    >
+      <div className="container-task-date">
+        <div className="task">{ task }</div>
+        <div className="date">{ `Data de criação: ${created}` }</div>
+      </div>
+      <div
+        className="task-status"
+        style={ { background: statusColor[status] } }
+      >
+        { status }
+      </div>
       <button
+        className="edit-button"
         type="button"
         onClick={ () => setTaskEdit(taskData) }
       >
-        Editar
+        <img className="edit-img" src={ editIcon } alt="Icone de editar" />
       </button>
       <button
+        className="delete-button"
         type="button"
         onClick={ clickDelete }
       >
-        Excluir
+        <img className="delete-img" src={ deleteIcon } alt="Icone de excluir" />
       </button>
     </li>
   );
--- task-organize-frontend/src/pages/Home.js
+++ task-organize-frontend/src/pages/Home.js
@@ -3,23 +3,31 @@ import React from 'react';
 import TaskInput from '../components/TaskInput';
 import TaskList from '../components/TaskList';
 import Filters from '../components/Filters';
+import image from '../images/orginizer.png';
+
+import '../styles/home.css';
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <header>
         <h1>
-          <span>TASK</span>
-          <span>ORGANIZE</span>
+          <span className="title-task">TASK</span>
+          <span className="title-orginizer">ORGANIZER</span>
         </h1>
         <Filters />
       </header>
-      <TaskInput />
-      <TaskList />
-      <footer>
-        <span>Projeto desenvolvido no curso da Trybe</span>
-        <span>Renata Nunes - 2022</span>
-      </footer>
+      <div className="tasks">
+        <TaskInput />
+        <div className="tasks-image">
+          <TaskList />
+          <img className="main-img" src={ image } alt="Imagem ilustrativa" />
+        </div>
+        <footer>
+          <span>Projeto desenvolvido no curso da Trybe</span>
+          <span>Renata Nunes - 2022</span>
+        </footer>
+      </div>
     </div>
   );
 }
